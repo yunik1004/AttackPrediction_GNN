608f2cbbcfb0d4be0e9667a897862df7c102886c
yunik1004 <inkyuhak@gmail.com>
2020-09-10 15:11:46 +0900

    Add validation

--- src/dataset.py
+++ src/dataset.py
@@ -96,3 +96,72 @@ class TCPNetworkTrainDataset(Dataset):
     def get(self, idx):
         data = torch.load(os.path.join(self.processed_dir, f"train_{idx:03d}.pt"))
         return data
+
+
+class TCPNetworkValidDataset(Dataset):
+    """Validation dataset class for loading the TCP networks
+    """
+
+    def __init__(self):
+        super().__init__(ROOT_DATASET, None, None, None)
+
+    @property
+    def raw_dir(self):
+        return os.path.join(self.root, "raw", "valid_query")
+
+    @property
+    def processed_dir(self):
+        return os.path.join(self.root, "processed", "valid")
+
+    @property
+    def raw_file_names(self):
+        return os.listdir(self.raw_dir)
+
+    @property
+    def processed_file_names(self):
+        return list(
+            map(
+                lambda x: f"valid_{Path(x).stem.split('_')[-1]}.pt", self.raw_file_names
+            )
+        )
+
+    def download(self):
+        pass
+
+    def process(self):
+        answer_dir = os.path.join(self.root, "raw", "valid_answer")
+
+        for raw_path in self.raw_paths:
+            rid = Path(raw_path).stem.split("_")[-1]
+
+            answer_path = os.path.join(answer_dir, f"valid_answer_{rid}.txt")
+            with open(answer_path, "r") as file:
+                answer_raw = file.readline().split()
+
+            graph_feature = torch.zeros(1, NUM_ATTACK_TYPES, dtype=torch.float)
+
+            if not answer_raw:
+                graph_feature[0, 0] = 1
+
+            for attack in answer_raw:
+                graph_feature[0, ATTACK_DICT[attack]] = 1
+
+            dataframe = pd.read_csv(raw_path, sep="\t", header=None)
+
+            edge_index = torch.from_numpy(dataframe.iloc[:, 0:2].values).long()
+            node_features = torch.ones(TCPNETWORK_NUM_NODES, 1, dtype=torch.float)
+
+            data = Data(
+                x=node_features, edge_index=edge_index.t().contiguous(), y=graph_feature
+            )
+
+            torch.save(data, os.path.join(self.processed_dir, f"valid_{rid}.pt"))
+
+            # exit(1)
+
+    def len(self):
+        return len(self.raw_file_names)
+
+    def get(self, idx):
+        data = torch.load(os.path.join(self.processed_dir, f"valid_{idx:03d}.pt"))
+        return data
--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division
 import matplotlib.pyplot as plt
 import torch
 from torch_geometric.data import DataLoader
-from dataset import TCPNetworkTrainDataset
+from dataset import TCPNetworkTrainDataset, TCPNetworkValidDataset
 from gnn import TCPNet
 
 
@@ -14,20 +14,25 @@ if __name__ == "__main__":
 
     HIDDEN_CHANNELS = 256
     BATCH_SIZE = 8
-    NUM_EPOCHS = 100
+    NUM_EPOCHS = 1000
     LEARNING_RATE = 0.01
+    VALID_FREQ = 100
 
     model = TCPNet(hidden_channels=HIDDEN_CHANNELS).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
     criterion = torch.nn.BCEWithLogitsLoss()
 
     train_dataset = TCPNetworkTrainDataset()
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    valid_dataset = TCPNetworkValidDataset()
 
-    loss_list = list()
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False)
+
+    train_loss_list = list()
+    valid_loss_list = list()
 
     for epoch in range(1, NUM_EPOCHS + 1):
-        total_loss = 0
+        total_train_loss = 0
 
         model.train()
         for data in train_loader:
@@ -39,11 +44,41 @@ if __name__ == "__main__":
             optimizer.step()
             optimizer.zero_grad()
 
-            total_loss += loss.item()
+            total_train_loss += loss.item()
 
-        average_loss = total_loss / len(train_loader)
-        print(f"Epoch {epoch}: loss = {average_loss}")
-        loss_list.append(average_loss)
+        average_train_loss = total_train_loss / len(train_loader)
+        print(f"Epoch {epoch}: train loss = {average_train_loss}")
+        train_loss_list.append(average_train_loss)
 
-    plt.plot(list(range(1, NUM_EPOCHS + 1)), loss_list)
+        if epoch % VALID_FREQ == 0:
+            total_valid_loss = 0
+
+            model.eval()
+            for data in valid_loader:
+                data = data.to(device)
+                out = model(data)
+
+                loss = criterion(out, data.y)
+                total_valid_loss += loss.item()
+
+            average_valid_loss = total_valid_loss / len(valid_loader)
+            print(f"valid loss = {average_valid_loss}")
+            valid_loss_list.append(average_valid_loss)
+
+    plt.plot(
+        list(range(1, NUM_EPOCHS + 1)),
+        train_loss_list,
+        label="Train loss",
+        color="blue",
+    )
+    plt.plot(
+        list(map(lambda x: x * VALID_FREQ, list(range(1, len(valid_loss_list) + 1)))),
+        valid_loss_list,
+        label="Valid loss",
+        color="red",
+    )
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.legend()
     plt.show()
